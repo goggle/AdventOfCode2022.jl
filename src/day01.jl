78297ccd4003e0c5ffe09e3e4746c604f4ce8855
Alexander Seiler <seileralex@gmail.com>
2022-12-01 01:10:47 +0100

    Add basic package structure

---
+++ src/day01.jl
@@ -0,0 +1,9 @@
+module Day01
+
+using AdventOfCode2022
+
+function day01(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
+    return [0, 0]
+end
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AdventOfCode2022
+using Test
+
+@testset "Day 1" begin
+    @test AdventOfCode2022.Day01.day01() == [0, 0]
+end
