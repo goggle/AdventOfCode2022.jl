20c23c251dde660a84f1eed07ef688bb285e0ed6
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:05:36 +0200

    [Day 16] Format

--- src/day16.jl
+++ src/day16.jl
@@ -8,10 +8,10 @@ function day16(input::String = readInput(joinpath(@__DIR__, "..", "data", "day16
     walking_dists = walking_distances(valves)
     positive_flow_mask = flows .> 0
     dists = walking_dists[positive_flow_mask, positive_flow_mask]
-    p1, _ = part1(walking_dists, positive_flow_mask, dists, flows, startidx, 30, Set{UInt8}(1:count(flows .> 0)), Dict{Int,Int}())
+    p1, _ = part1(walking_dists, positive_flow_mask, dists, flows, startidx, 30, Set{UInt8}(1:count(flows .> 0)), Dict{Int, Int}())
 
     p2 = 0
-    _, scores = part1(walking_dists, positive_flow_mask, dists, flows, startidx, 26, Set{UInt8}(1:count(flows .> 0)), Dict{Int,Int}(); store_scores = true)
+    _, scores = part1(walking_dists, positive_flow_mask, dists, flows, startidx, 26, Set{UInt8}(1:count(flows .> 0)), Dict{Int, Int}(); store_scores = true)
     test_value = p1 ÷ 2
     for (k1, v1) ∈ scores
         v1 < test_value && continue
@@ -19,22 +19,22 @@ function day16(input::String = readInput(joinpath(@__DIR__, "..", "data", "day16
             if k1 & k2 == 0  # set of opened valve indices are disjoint
                 if p2 < v1 + v2
                     p2 = v1 + v2
-                end 
+                end
             end
         end
     end
     return [p1, p2]
 end
 
-function part1(walking_dists::Matrix{Int}, positive_flow_mask::BitVector, dists::Matrix{Int}, flows::Vector{Int}, startidx::Int, starttime::Int, available::Set{UInt8}, scores::Dict{Int,Int}; store_scores = false)
-    q = Deque{Tuple{Int,Int,Int,Set{UInt8}}}()
+function part1(walking_dists::Matrix{Int}, positive_flow_mask::BitVector, dists::Matrix{Int}, flows::Vector{Int}, startidx::Int, starttime::Int, available::Set{UInt8}, scores::Dict{Int, Int}; store_scores = false)
+    q = Deque{Tuple{Int, Int, Int, Set{UInt8}}}()
 
     # Initialization of the search: Walk to all the valves with positive pressure
     # release, open them, and add these states to the queue.
     for i ∈ findall(flows .> 0)
-        idx = (flows .> 0)[1:i] |> sum
+        idx = (flows.>0)[1:i] |> sum
         idx ∉ available && continue
-        time_left = starttime - walking_dists[startidx,i] - 1
+        time_left = starttime - walking_dists[startidx, i] - 1
         score = time_left * flows[i]
         open_idx = Set{UInt8}(idx)
         push!(q, (time_left, score, idx, open_idx))
@@ -89,7 +89,7 @@ function part1(walking_dists::Matrix{Int}, positive_flow_mask::BitVector, dists:
     return max_score, scores
 end
 
-function walking_distances(valves::Dict{Int,Vector{Int}})
+function walking_distances(valves::Dict{Int, Vector{Int}})
     # Calculates the walking distances between the valves by using
     # the Floyd–Warshall algorithm (https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm)
     n = length(valves)
@@ -116,10 +116,10 @@ end
 
 function parse_input(input::AbstractString)
     flows = Vector{Int}()
-    valves = Dict{String,Vector{String}}()
+    valves = Dict{String, Vector{String}}()
     r = r"Valve\s(\w{2})\shas\sflow\srate=(\d+);\stunnels?\sleads?\sto\svalves?\s(.+)$"
-    links = Dict{String,Int}()
-    for (i,line) in enumerate(eachsplit(rstrip(input), "\n"))
+    links = Dict{String, Int}()
+    for (i, line) in enumerate(eachsplit(rstrip(input), "\n"))
         m = match(r, line)
         push!(flows, parse(Int, m.captures[2]))
         valve = m.captures[1]
@@ -128,7 +128,7 @@ function parse_input(input::AbstractString)
         links[valve] = i
     end
 
-    graph = Dict{Int,Vector{Int}}()
+    graph = Dict{Int, Vector{Int}}()
     for (key, v) ∈ valves
         graph[links[key]] = [links[vi] for vi ∈ v]
     end
