fa28b07a684ebf15b9a64580a2d243fcb8c48fbf
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:03:47 +0200

    [Day 10] Format

--- src/day10.jl
+++ src/day10.jl
@@ -5,13 +5,13 @@ using AdventOfCode2022
 function day10(input::String = readInput(joinpath(@__DIR__, "..", "data", "day10.txt")))
     data = map(x -> length(x) == 2 ? (String(x[1]), parse(Int, x[2])) : (String(x[1]), 0), split.(eachsplit(rstrip(input), "\n")))
     d = collect_values(data)
-    p1 = sum(d[x] * x for x ∈ (20 + 40*k for k ∈ 0:5))
+    p1 = sum(d[x] * x for x ∈ (20 + 40 * k for k ∈ 0:5))
     p2 = part2(d)
     return [p1, p2 |> permutedims |> generate_image]
 end
 
-function collect_values(data::Vector{Tuple{String,Int}})
-    d = Dict{Int,Int}()
+function collect_values(data::Vector{Tuple{String, Int}})
+    d = Dict{Int, Int}()
     x = 1
     cycle = 0
     d[0] = 1
@@ -29,7 +29,7 @@ function collect_values(data::Vector{Tuple{String,Int}})
     return d
 end
 
-function part2(d::Dict{Int,Int})
+function part2(d::Dict{Int, Int})
     image = fill('.', 6, 40)
     for cycle ∈ 1:240
         if mod1(cycle, 40) ∈ d[cycle]:d[cycle]+2
