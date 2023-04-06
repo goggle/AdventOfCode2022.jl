73a3307ff2a1b918a56110fbadaaf130f312efb8
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:04:30 +0200

    [Day 12] Format

--- src/day12.jl
+++ src/day12.jl
@@ -6,7 +6,7 @@ function day12(input::String = readInput(joinpath(@__DIR__, "..", "data", "day12
     startindex, endindex, heightmap = parse_input(input)
     revcosts = reverse_costs(endindex, heightmap)
     p1 = revcosts[startindex]
-    p2 = revcosts[findall(x->x==0, heightmap)] |> minimum
+    p2 = revcosts[findall(x -> x == 0, heightmap)] |> minimum
     return [p1, p2]
 end
 
@@ -33,18 +33,18 @@ end
 function neighbours(index::CartesianIndex{2}, M::Int, N::Int)
     neighb = CartesianIndex{2}[]
     i, j = index.I
-    i-1 >= 1 && push!(neighb, CartesianIndex(i-1,j))
-    j-1 >= 1 && push!(neighb, CartesianIndex(i, j-1))
-    i+1 <= M && push!(neighb, CartesianIndex(i+1, j))
-    j+1 <= N && push!(neighb, CartesianIndex(i, j+1))
+    i - 1 >= 1 && push!(neighb, CartesianIndex(i - 1, j))
+    j - 1 >= 1 && push!(neighb, CartesianIndex(i, j - 1))
+    i + 1 <= M && push!(neighb, CartesianIndex(i + 1, j))
+    j + 1 <= N && push!(neighb, CartesianIndex(i, j + 1))
     return neighb
 end
 
 function parse_input(input::AbstractString)
     heightmap = map(x -> x[1], reduce(vcat, permutedims.(map(x -> split(x, ""), split(input)))))
-    startindex = findfirst(x->x=='S', heightmap)
-    endindex = findfirst(x->x=='E', heightmap)
-    replace!(heightmap, 'S'=>'a', 'E'=>'z')
+    startindex = findfirst(x -> x == 'S', heightmap)
+    endindex = findfirst(x -> x == 'E', heightmap)
+    replace!(heightmap, 'S' => 'a', 'E' => 'z')
     return startindex, endindex, heightmap .- 'a'
 end
 
