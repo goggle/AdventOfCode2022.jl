37886fbd52aa7c698b6a21843784b9b610fb9427
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:01:39 +0200

    [Day 1] Format

--- src/day01.jl
+++ src/day01.jl
@@ -4,7 +4,7 @@ using AdventOfCode2022
 
 function day01(input::String = readInput(joinpath(@__DIR__, "..", "data", "day01.txt")))
     sums = map(x -> parse.(Int, eachsplit(x)) |> sum, eachsplit(input, "\n\n"))
-    partialsort!(sums, 1:3, rev=true)
+    partialsort!(sums, 1:3, rev = true)
     return [sums[1], sums[1:3] |> sum]
 end
 
