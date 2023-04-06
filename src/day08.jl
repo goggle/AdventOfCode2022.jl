743a80ab067168fa2719e7a77d1fff628da8dcfa
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:03:25 +0200

    [Day 8] Format

--- src/day08.jl
+++ src/day08.jl
@@ -4,45 +4,45 @@ using AdventOfCode2022
 
 function day08(input::String = readInput(joinpath(@__DIR__, "..", "data", "day08.txt")))
     heightmap = reduce(vcat, permutedims.(map(x -> parse.(Int, split(x, "")), split(input))))
-    return [part1(heightmap), part2(heightmap)]    
+    return [part1(heightmap), part2(heightmap)]
 end
 
 function part1(heightmap::Matrix{Int})
     visible = zeros(Bool, size(heightmap))
-    visible[1,:] .= visible[end,:] .= visible[:,1] .= visible[:,end] .= true
-    for i = 2:size(visible)[1] - 1
+    visible[1, :] .= visible[end, :] .= visible[:, 1] .= visible[:, end] .= true
+    for i ∈ 2:size(visible)[1]-1
         M = heightmap[i, 1]
-        for j = 2:size(visible)[2] - 1
-            if heightmap[i,j] > M
-                visible[i,j] = true
-                M = heightmap[i,j]
+        for j ∈ 2:size(visible)[2]-1
+            if heightmap[i, j] > M
+                visible[i, j] = true
+                M = heightmap[i, j]
             end
         end
     end
-    for j = 2:size(visible)[2] - 1
+    for j ∈ 2:size(visible)[2]-1
         M = heightmap[1, j]
-        for i = 2:size(visible)[1] - 1
-            if heightmap[i,j] > M
-                visible[i,j] = true
-                M = heightmap[i,j]
+        for i ∈ 2:size(visible)[1]-1
+            if heightmap[i, j] > M
+                visible[i, j] = true
+                M = heightmap[i, j]
             end
         end
     end
-    for i = size(visible)[1]-1:-1:2
+    for i ∈ size(visible)[1]-1:-1:2
         M = heightmap[i, end]
-        for j = size(visible)[2]-1:-1:2
-            if heightmap[i,j] > M
-                visible[i,j] = true
-                M = heightmap[i,j]
+        for j ∈ size(visible)[2]-1:-1:2
+            if heightmap[i, j] > M
+                visible[i, j] = true
+                M = heightmap[i, j]
             end
         end
     end
-    for j = size(visible)[2]-1:-1:2
+    for j ∈ size(visible)[2]-1:-1:2
         M = heightmap[end, j]
-        for i = size(visible)[1]-1:-1:2
-            if heightmap[i,j] > M
-                visible[i,j] = true
-                M = heightmap[i,j]
+        for i ∈ size(visible)[1]-1:-1:2
+            if heightmap[i, j] > M
+                visible[i, j] = true
+                M = heightmap[i, j]
             end
         end
     end
@@ -51,8 +51,8 @@ end
 
 function part2(heightmap::Matrix{Int})
     scoremap = zeros(Int, size(heightmap))
-    for i = 2:size(heightmap)[1]-1
-        for j = 2:size(heightmap)[2]-1
+    for i ∈ 2:size(heightmap)[1]-1
+        for j ∈ 2:size(heightmap)[2]-1
             up = down = left = right = 0
             M = heightmap[i, j]
             k, l = i, j
@@ -63,7 +63,7 @@ function part2(heightmap::Matrix{Int})
             end
 
             k, l = i, j
-            while k <= size(heightmap)[1]-1
+            while k <= size(heightmap)[1] - 1
                 k += 1
                 down += 1
                 heightmap[k, l] >= M && break
@@ -77,13 +77,13 @@ function part2(heightmap::Matrix{Int})
             end
 
             k, l = i, j
-            while l <= size(heightmap)[2]-1
+            while l <= size(heightmap)[2] - 1
                 l += 1
                 right += 1
                 heightmap[k, l] >= M && break
             end
 
-            scoremap[i,j] = up * down * left * right
+            scoremap[i, j] = up * down * left * right
         end
     end
     return scoremap |> maximum
