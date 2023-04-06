cc9defcd3152b3aec5f48f602148c08055a1700c
Alexander Seiler <seileralex@gmail.com>
2023-04-06 04:02:51 +0200

    [Day 6] Format

--- src/day06.jl
+++ src/day06.jl
@@ -3,7 +3,7 @@ module Day06
 using AdventOfCode2022
 
 function day06(input::String = readInput(joinpath(@__DIR__, "..", "data", "day06.txt")))
-    return [findfirst(marker(input, i, k) for i=1:length(input)-k+1) + k - 1 for k ∈ (4, 14)]
+    return [findfirst(marker(input, i, k) for i ∈ 1:length(input)-k+1) + k - 1 for k ∈ (4, 14)]
 end
 
 marker(s::AbstractString, i::Int, l::Int) = (s[i:i+l-1] |> unique |> length) == l
